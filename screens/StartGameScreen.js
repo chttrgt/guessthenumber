9f13c6b6777c398353dadd1e4ba09d41c271e773
doommoob <doom.white@yaani.com>
2024-10-03 15:03:44 +0300

    Created screens structor...

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({});
