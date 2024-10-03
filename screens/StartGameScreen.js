ee1463f2be6414ec392bd7ae3dd8e4993db34b2d
doommoob <doom.white@yaani.com>
2024-10-03 15:29:32 +0300

    Created custom Button component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,14 +1,30 @@
 import React from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 };
 
 export default StartGameScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 10,
+    elevation: 15, // for only android
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 6,
+  },
+});
